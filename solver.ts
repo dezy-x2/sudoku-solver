824fb6c6f6c647b51577104b100d1a24db1ec092
Daniel Desmond <df.dezmond9@gmail.com>
2021-08-20 13:04:35 -0400

    commented the makeGrid func bc its tad confusing

--- solver.ts
+++ solver.ts
@@ -15,21 +15,27 @@ function getDimensions(puzzle: number[][]): [number, number] {
 }
 
 function makeGrid(puzzle: number[][]): { [key: string]: number[] } {
+  // need to find the dimensions of the puzzle
   const [rows, cols] = getDimensions(puzzle);
+  // each grid is a array with the key being its coordinate
   const grid: { [key: string]: number[] } = {};
+  // iterate through the rows
   for (let g = 0; g < rows; g += 3) {
+    // this arr is used to keep arrs of a rows grids
     let nums: number[][] = [];
+    // iterate through the columns
     for (let i: number = 0; i < cols; i += 3) {
-      console.log(g, i, "outer====");
+      // this arr holds one grid and is added to the nums arr
       let temp: number[] = [];
+      // these two loops go through each num in the grid
       for (let j: number = g; j < g + 3; j++) {
         for (let k: number = i; k < i + 3; k++) {
-          console.log(j, k, "inner");
           temp.push(puzzle[j][k]);
         }
       }
       nums.push(temp);
     }
+    // this loop breaks down the nums arr into the grid
     for (let x = 0; x < nums.length; x++) {
       const key: string = `${g + x}`;
       grid[key] = nums[x];
