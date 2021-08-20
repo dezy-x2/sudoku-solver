68bf82130a23beb82e01be5daede2208655ed371
Daniel Desmond <df.dezmond9@gmail.com>
2021-08-19 21:44:47 -0400

    decided to use typescript instead of js

---
+++ solver.ts
@@ -0,0 +1,12 @@
+const puzzle : number[][] = [
+  [9, null, null, 6, 4, null, null, null, 3],
+  [2, 7, null, null, 9, null, 5, 8, null],
+  [null, 1, null, 5, 8, null, null, null, null],
+  [null, 9, null, null, null, null, 7, null, null],
+  [null, null, 7, 9, 6, 5, 8, null, null],
+  [null, null, 2, null, null, null, null, 4, null],
+  [null, null, 2, null, null, null, null, 4, null],
+  [null, null, null, null, 5, 3, null, 6, null],
+  [null, 5, 1, null, 7, null, null, 2, 8],
+  [4, null, null, null, 1, 6, null, null, 5],
+];
