a68cc93ae122d0d89f6f9e802f832e74b3324b2d
Daniel Desmond <df.dezmond9@gmail.com>
2021-09-29 16:23:19 -0400

    forgot to compile so this file had to change

--- solver.js
+++ solver.js
@@ -112,6 +112,11 @@ function legalPlacement(col, row, num, sudoku) {
         !numPresent(grid, num) &&
         sudoku[row][col] === null);
 }
+/**
+ * @param {number} colCount number of items in the col
+ * @param {number} rowCount number of items in the row
+ * @returns {number[]} it returns 2d arr will null values
+ */
 function generateEmptyPuzzle(colCount, rowCount) {
     var final = [];
     for (var i = 0; i < rowCount; i++) {
@@ -123,16 +128,24 @@ function generateEmptyPuzzle(colCount, rowCount) {
     }
     return final;
 }
-//! theres definitely some errors here but im tired
+/**
+ *
+ * @param depth the amount of numbers you want to put in the puzzle (1-9)
+ * @returns a puzzle with the amount of numbers you want
+ */
 function randomPlacer(depth) {
+    // creates the puzzle that we will be populating
     var sudoku = generateEmptyPuzzle(9, 9);
     var _a = getDimensions(sudoku), row = _a[0], col = _a[1];
     var count = 0;
+    // i is the the number that we are trying to place in the sudoku
     for (var i = 1; i <= depth; i++) {
+        // keep trying to place i until it has been placed 9 times
         while (count < 9) {
+            // generate a random row and column
             var rowToPlace = Math.floor(Math.random() * row);
             var colToPlace = Math.floor(Math.random() * col);
-            console.log(rowToPlace, colToPlace, legalPlacement(colToPlace, rowToPlace, i, sudoku), count, sudoku[rowToPlace][colToPlace]);
+            // we need to check if it is legal to place the number here
             if (legalPlacement(colToPlace, rowToPlace, i, sudoku)) {
                 sudoku[rowToPlace][colToPlace] = i;
                 count++;
